ab32e4eb2059b25b0ac796bb7e28b589156fa207
Andrey <marvel.dnepr@gmail.com>
2019-03-17 20:11:18 +0200

    Initial Commit

---
+++ myEvent/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myEvent
+//
+//  Created by Andrey on 17.03.2019.
+//  Copyright © 2019 Andrey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
